90a2c53428f5888f6e433044a79d19a8d416894f
Robby Dore <Rdore88@gmail.com>
2017-04-12 14:08:34 -0400

    Created a function to add a new job based on the parameters listed in the assignment

--- server/routes/jobs.routes.js
+++ server/routes/jobs.routes.js
@@ -4,8 +4,16 @@ let jobs = [{ id: '1', company: 'rosaCO', link: 'rosaCO@company.com', notes: 'Be
 console.log(jobs);
 
 jobsRouter.get("/", function showAllJobs(req, res, next) {
-
   res.json(jobs);
 });
+
+function addAJob(req, res, next) {
+  console.log("This shows req.body", req.body);
+
+  jobs.push(req.body);
+
+  res.json({message: 'I sucessfully posted to the rosaCO site'});
+}
+jobsRouter.post("/", addAJob);
 
 module.exports = jobsRouter;

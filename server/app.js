64fbb3eed0d757e2bf979cb4a933f1986724898a
Robby Dore <Rdore88@gmail.com>
2017-04-12 13:46:27 -0400

    Set up and ran first end point.

--- server/app.js
+++ server/app.js
@@ -3,3 +3,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 let app = express();
+// app.use(bodyParser.json());
+
+app.use( "/jobs", require("./routes/jobs.routes") );
+
+
+
+app.listen(3000, function serverIsWorking() {
+  console.log("This should be running");
+});

621851837e7eed2e845f525c48452e909bbbc49f
Robby Dore <Rdore88@gmail.com>
2017-04-12 14:36:49 -0400

    Created a forEach loop in the function to get back desired output

--- server/routes/jobs.routes.js
+++ server/routes/jobs.routes.js
@@ -1,10 +1,14 @@
 const jobsRouter = require('express').Router();
 
 let jobs = [{ id: '1', company: 'rosaCO', link: 'rosaCO@company.com', notes: 'Best Company Ever', createTime: Date.now()}];
-console.log(jobs);
 
 jobsRouter.get("/", function showAllJobs(req, res, next) {
-  res.json(jobs);
+  let jobsCollection = [];
+  jobs.forEach(function infoWeNeed(job) {
+    jobsCollection.push({id: job.id, company: job.company, link: job.link});
+  });
+
+  res.json(jobsCollection);
 });
 
 /**

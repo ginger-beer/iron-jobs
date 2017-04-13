c1f0c08ea6375ececd7cdf4bd04b3c1c61380ea5
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-13 09:50:00 -0400

    Updated doc blocks. Added additional error handling as well as a future check to put in. Refectored a function to use map rather than forEach. Created a new job using re.body's componets rather than putting a label on it and changing it.

--- server/routes/jobs.routes.js
+++ server/routes/jobs.routes.js
@@ -2,29 +2,45 @@ const jobsRouter = require('express').Router();
 
 let jobs = [{ id: '1', company: 'rosaCO', link: 'rosaCO@company.com', notes: 'Best Company Ever', createTime: Date.now()}];
 
-jobsRouter.get("/", function showAllJobs(req, res, next) {
+/**
+ * Show all of the jobs
+ * @param  {Object}     res  Shows all of the jobs
+ * @param  {Function}   next
+ * @return {void}
+ */
+function showAllJobs(req, res, next) {
   let jobsCollection = [];
-  jobs.forEach(function infoWeNeed(job) {
-    jobsCollection.push({id: job.id, company: job.company, link: job.link});
-  });
-  res.json(jobsCollection);
-});
+
+  if (!Array.isArray(jobs)) {
+      let err = new Error("Jobs is no longer an array");
+      err.status = 500;
+      return next(err);
+  }
+
+  res.json(jobs.map(function infoWeNeed(job) {
+    return ({id: job.id, company: job.company, link: job.link});
+  }));
+}
+jobsRouter.get("/", showAllJobs);
 
 /**
   * This function adds a new job to the current array of jobs
-  * @param {Object} req Must have a body like: {company: String, link: String, notes: String}
-  * @param {Object} res The response will contain the message: {message: 'I sucessfully posted to the rosaCO site'}
+  * @param  {Object}    req Must have a body like: {company: String, link: String, notes: String}
+  * @param  {Object}    res The response will contain the message: {message: 'I sucessfully posted to the rosaCO site'}
+  * @param  {Function}  next
+  * @return {void}
 **/
 function addAJob(req, res, next) {
   console.log("This shows req.body", req.body);
 
-  if (!(req.body.company && req.body.link && req.body.notes)) {
+  if (!req.body.company || !req.body.link || !req.body.notes) {
     let err = new Error("Please give required job information");
     err.status = 400;
     return next(err);
   }
+  //TODO check for empty string entry as company, link, notes
 
-  let newJob = req.body;
+  let newJob = { company: req.body.company, link: req.body.link, notes: req.body.notes };
   newJob.createTime = Date.now();
   newJob.id = JSON.stringify(Date.now());
   jobs.push(newJob);
@@ -35,8 +51,9 @@ jobsRouter.post("/", addAJob);
 
 /**
   * Allows you to show a specific job based on individual id
-  * @param {Object} req Identifies the specific job we would like to view based on id
-  * @param {Object} res Displays the job we slect via id
+  * @param  {Object} req Identifies the specific job we would like to view based on id
+  * @param  {Object} res Displays the job we slect via id
+  * @return {void}
 **/
 function showSpecificJob(req, res, next) {
   let correctJob = jobs.filter(function specificJobId(job) {
@@ -48,8 +65,9 @@ jobsRouter.get("/:id", showSpecificJob);
 
 /**
   * Allows you to delete a specific job via its individual id
-  * @param {Object} req Identifies the job to be deletd
-  * @param {Object} res Displays the job to be deleted
+  * @param  {Object} req Identifies the job to be deletd
+  * @param  {Object} res Displays the job to be deleted
+  * @return {void}
 **/
 function deleteSpecificJob(req, res, next) {
   jobs.find(function jobToDelete(job, index) {

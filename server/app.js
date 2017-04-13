61d39fa3d0d6d92b600c6234d86711a39b679940
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-13 09:17:07 -0400

    Updated file so all quotes are double quotes.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 let app = express();
 app.use(bodyParser.json());

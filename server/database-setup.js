fe30bb0f1dd9c145e5e498fdf38d03c65b122762
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-13 14:15:56 -0400

    Set up connection to database and check for if connection failed.

--- server/database-setup.js
+++ server/database-setup.js
@@ -1,2 +1,14 @@
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+if (!process.env.MY_DB_LOCATION) {
+    console.error("No database detected!");
+    process.exit(1046);
+}
+
+mongoose.connect(process.env.MY_DB_LOCATION);
+
+mongoose.connection.on("error", function handleDBError(err){
+    console.error("Database error", err);
+    process.exit(1046);
+});

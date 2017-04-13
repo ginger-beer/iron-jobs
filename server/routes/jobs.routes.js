6a66603a8fb5868e2c5e6f54c85cbe299355d328
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-04-13 14:52:26 -0400

    Required job model and removed fake data

--- server/routes/jobs.routes.js
+++ server/routes/jobs.routes.js
@@ -1,6 +1,6 @@
 const jobsRouter = require('express').Router();
 
-let jobs = [{ id: '1', company: 'rosaCO', link: 'rosaCO@company.com', notes: 'Best Company Ever', createTime: Date.now()}];
+const Job = require("../models/Job.model");
 
 /**
  * Show all of the jobs
